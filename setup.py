9794559add581bbb45329e8a16389eaf68086e64
Brandon M. Pace <brandonmpace@gmail.com>
2020-06-18 23:01:58 -0500

    Update version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="logcontrol",
-    version="0.1.0",
+    version="0.1.1",
     author="Brandon M. Pace",
     author_email="brandonmpace@gmail.com",
     description="A logger manager for Python programs",
